9a2ea8c07ab145b1ae286b7aee7701d88bb8b629
James Golz <jmgolz@gmail.com>
2020-08-31 17:08:31 -0400

    Add in front end delete ability

--- www/resources/js/components/TasksTableComponent.vue
+++ www/resources/js/components/TasksTableComponent.vue
@@ -3,10 +3,21 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Tasks - TODOs</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                         <table class="table table-striped table-bordered">
+                            <thead class="thead-dark">
+                                <th>Name</th>
+                                <th></th>
+                            </thead>
+                            <tbody>
+                                <tr v-for="task in tasks" :key="task.id">
+                                    <td>{{ task.task }}</td>
+                                    <td align="center"><button class="btn btn-danger btn-sm" @click.prevent="deleteTask(task.id)"><i class="fa fa-times" /> Delete</button></td>
+                                </tr>                
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -22,15 +33,19 @@
             };
         },
         mounted() {
-            // console.log('Component mounted.')
             this.getTasks();
         },
         methods: {
             getTasks() {
                 return axios.get('/api/tasks')
                     .then(response => {
-                        console.log(response);
+                        this.tasks = response.data;
                     }).catch(console.error)
+            },
+            deleteTask(id) {
+                return axios.post('/api/tasks/' + id, {_method: 'DELETE'})
+                    .then(this.getTasks)
+                    .catch(console.error);
             }
         }
     }
